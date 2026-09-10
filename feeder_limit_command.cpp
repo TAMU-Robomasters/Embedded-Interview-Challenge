7fc129580b0030ba15a5ef18285455dbac2f9f5f
Kenny Zheng <zhengk@tamu.edu>
2026-09-10 00:27:13 -0500

    Jordan Zamar Submission

--- feeder_limit_command.cpp
+++ feeder_limit_command.cpp
@@ -17,10 +17,20 @@ namespace src::Feeder {
 Define any variables you need here! 
 We have provided some here to give you sensor data and other useful things
 */
-bool limitPressed = false;
-bool wantToShoot = false;
-float currRPM = 0.0;
-
+bool limitPressed = false; //if ball is present
+bool wantToShoot = false; 
+float currRPM = 0.0; //rpm of feeder
+//variables i added
+//All potential states of action
+bool Loading = false;
+bool Kicking = false;
+bool Jammed = false;
+float heatLevel = 0.0;
+int lastHeatUpdate = 0;
+// all arbitrary timings based on video in github
+const int loadTime = 1000;
+const int kickTime = 1200 ;
+const int desRPM = 12;
 
 //Subsystem declarations, do not touch these
 FeederLimitCommand::FeederLimitCommand(
@@ -49,6 +59,14 @@ void FeederLimitCommand::initialize() {
 
     //System time on initialization, might be useful for heat managment
     initialTime = tap::arch::clock::getTimeMilliseconds();
+
+    //I added this
+    //establishes initial states of action
+    Loading = false;
+    Kicking = false;
+    Jammed = false;
+    heatLevel = 0.0;
+    lastHeatUpdate = initialTime;
 }
 
 void FeederLimitCommand::execute() {
@@ -70,7 +88,63 @@ void FeederLimitCommand::execute() {
 
     //Write Here for Challenge 1, 2, 3!
     
+    //Overheat Mechanic
+    int currentTime = tap::arch::clock::getTimeMilliseconds();
+    int elapsed = currentTime - lastHeatUpdate;
+    //lower heat bound
+    if(heatLevel < 0) heatLevel = 0;
+    //heat depletion 10/1s = 10/1000ms = 1/100ms
+    if(elapsed > 0){
+        float decrement = elapsed/100.0f;
+        heatLevel -= decrement;
+        lastHeatUpdate = currentTime;
+    }
+    //Overheat state
+    bool overheat = (heatLevel >= 200);
+    //heat continues in shoot mechanic
+
+    //Shooting Mechanic
+    if(wantToShoot && limitPressed && !Loading && !Kicking && !Jammed && !overheat){
+        feeder->ForFeederMotorGroup(LOADER, &FeederSubsystem::activateFeederMotor);
+        timer1.restart(loadTime);
+        timer4.restart(0);
+        Loading = true;
+    }
+    //checking if loading done
+    if(Loading && timer1.isExpired() && limitPressed){
+        feeder->ForFeederMotorGroup(LOADER, &FeederSubsystem::deactivateFeederMotor);
+        Loading = false;
+        //start kicking once loading done
+        feeder->ForFeederMotorGroup(KICKER, &FeederSubsystem::activateFeederMotor);
+        timer2.restart(kickTime);
+        Kicking = true;
+    }
+    //checking if kicking done
+    if(Kicking && timer2.isExpired()){
+        feeder->ForFeederMotorGroup(KICKER, &FeederSubsystem::deactivateFeederMotor);
+        Kicking = false;
+        //increments heat once shot is shot
+        heatLevel += 100;
+        //upper heat bound
+        if(heatLevel > 200) heatLevel = 200;
+    }
     
+    //jamming mechanic
+    //if loading is looked slow we start jam counteraction
+    if(Loading && limitPressed && currRPM < desRPM && timer4.get()>100){
+        Jammed = true;
+        Loading = false;
+        //stop loading
+        feeder->ForFeederMotorGroup(LOADER, &FeederSubsystem::deactivateFeederMotor);
+        //start unjamming
+        feeder->ForFeederMotorGroup(LOADER, &FeederSubsystem::unjamFeederMotor);
+        timer3.restart(UNJAM_TIMER_MS);
+    }
+    //checks if unjamming is finished
+    if(Jammed && timer3.isExpired()){
+        feeder->ForFeederMotorGroup(LOADER, &FeederSubsystem::deactivateFeederMotor);
+        Jammed = false;
+    }
     
 }
 
