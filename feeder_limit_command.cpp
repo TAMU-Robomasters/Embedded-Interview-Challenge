4aa25928a48259fffcde5560fb8e3f616a53efe0
Kenny Zheng <zhengk@tamu.edu>
2026-09-10 00:30:25 -0500

    Parthiv Dumpa Submission

--- feeder_limit_command.cpp
+++ feeder_limit_command.cpp
@@ -19,7 +19,11 @@ We have provided some here to give you sensor data and other useful things
 */
 bool limitPressed = false;
 bool wantToShoot = false;
+bool loaded = false;
 float currRPM = 0.0;
+int currentHeat = 0;
+const int maxHeat = 200;
+const int heat_dis = 10;
 
 
 //Subsystem declarations, do not touch these
@@ -69,9 +73,8 @@ void FeederLimitCommand::execute() {
     currRPM = feeder->getCurrentRPM(0);
 
     //Write Here for Challenge 1, 2, 3!
-    
-    
-    
+
+    void Loaded_Kicker();
 }
 
 /*Declare any helper functions down here (make sure to include in hpp)*/
@@ -80,7 +83,70 @@ void FeederLimitCommand::execute() {
 //  do stuff
 //};
 
+void heat_dissapation {
+    //Should dissapate at a rate of 10 heat per second; 1 second = 1000 milliseconds
+    //Check if disspation runs all times globally
+    for (timer1.restart(0); timer1.getTimeMilliseconds() == 1000; ) {
+        currentHeat -= 10;
+    }
+}
 
+void confirm_loaded {
+    currRPM = feeder->getCurrentRPM(0);
+    if (feeder->ForFeederMotorGroup(LOADER, &FeederSubsystem::activateFeederMotor && currRPM > 0 && limitPressed)) {
+        loaded = true;
+    }
+    else {
+        loaded = false;
+        while (loaded == false) {
+            //Should give time for unjam to occur, then try to load again; if it fails again, repeat until successful
+            unjam_timer.restart(0);
+            while (unjam_timer.getTimeMilliseconds() < UNJAM_TIMER_MS) {
+                feeder->ForFeederMotorGroup(LOADER, &FeederSubsystem::unjamFeederMotor);
+            }
+            feeder ->ForFeederMotorGroup(LOADER, &FeederSubsystem::activateFeederMotor);
+            currRPM = feeder->getCurrentRPM(0);
+            if (feeder ->ForFeederMotorGroup(LOADER, &FeederSubsystem::activateFeederMotor && currRPM > 0 && limitPressed)) {
+                loaded = true;
+            }
+            else {loaded = false;}
+            limitPressed = feeder->getPressed();
+        }
+    }
+}
+
+void Loaded_Kicker() {
+    if (currentHeat > maxHeat) {
+        currentHeat = maxHeat;
+        wantToShoot = false;
+        while (currentHeat > maxHeat) {
+            heat_dissapation();
+        }
+    }
+    else if (currentHeat < 0) {
+        currentHeat = 0;
+    }
+    else {
+        //Only Loads
+        if (!limitPressed) {   
+            feeder->ForFeederMotorGroup(LOADER, &FeederSubsystem::activateFeederMotor)
+            limitPressed = feeder->getPressed();
+            confirm_loaded();
+        }
+        else if (limitPressed && loaded) {
+            if (wantToShoot) {
+                //Shoots then adds heat to the system; so load then fire immediately after
+                feeder->ForFeederMotorGroup(KICKER, &FeederSubsystem::activateFeederMotor);
+                currentHeat +=100;
+            }
+        }
+        else {
+            feeder->ForFeederMotorGroup(ALL, &FeederSubsystem::deactivateFeederMotor);
+        }
+        heat_dissapation();
+    }  
+
+}
 
 //Do not touch these functions, they are required for the command to work properly
 void FeederLimitCommand::end(bool) { feeder->ForFeederMotorGroup(ALL, &FeederSubsystem::deactivateFeederMotor); }
